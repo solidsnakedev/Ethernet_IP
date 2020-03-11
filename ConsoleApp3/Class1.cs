0aedd1422f25d6cc8fc6f376d10a1dec0f63d07f
Snake <jrodriguezp7@gmail.com>
2020-03-11 12:20:14 -0400

    Added comments

--- ConsoleApp3/Class1.cs
+++ ConsoleApp3/Class1.cs
@@ -9,6 +9,8 @@ namespace ConsoleApp3
         public int x;
         public int y;
        // public int result;
+
+            // test
         public Class1(int x, int y, int z)
         {
             this.x = x;
